21b06b231f3a5d405dcba3e70df48f22bea1f604
Darshan2024 <darshanugale10@gmail.com>
2025-10-08 22:19:23 -0400

    First version of main page

--- src/main/java/edu/dugale/LibraryManagementSystem/controllers/HomeController.java
+++ src/main/java/edu/dugale/LibraryManagementSystem/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package edu.dugale.LibraryManagementSystem.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +9,17 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/signin")
+    public String signin(Model model){
+        model.addAttribute("heading", "Sign In");
+        return "under-construction";
+    }
+
+    @GetMapping("/books")
+    public String books(Model model){
+        model.addAttribute("heading", "View Books");
+        return "under-construction";
     }
 }
